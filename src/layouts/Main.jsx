4452b555c509597a103646776568599e2f745edc
ForhadRoben <i.rforhad@gmail.com>
2023-05-04 01:53:14 +0600

    navbar-footer-responsive-for-shared-pages

---
+++ src/layouts/Main.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from '../pages/shared/header/header';
+import Footer from '../pages/shared/footer/Footer';
+
+const Main = () => {
+    return (
+        <div>
+            <Header></Header>
+            <Footer></Footer>
+
+
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/pages/shared/header/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
+
+const Header = () => {
+    return (
+        <>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light">
+                <Container>
+                    <Navbar.Brand href="#home" className='fs-2 font'><span className='text-info '>Turkish </span><span className='text-warning'>Dinner</span></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="ms-auto d-flex justify-content-start align-items-center">
+                            <Nav.Link href="#features">Home</Nav.Link>
+                            <Nav.Link href="#pricing">Blogs</Nav.Link>
+                            <Nav.Link href="#deets">Profile</Nav.Link>
+                            <Nav.Link href="#memes">
+                                <Button variant="info">Login</Button>
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </>
+    );
+};
+
+export default Header;
